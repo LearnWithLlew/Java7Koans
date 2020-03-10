068535f9c60cde9dca8f2f4e0d10d2cf8e9ff0e9
Llewellyn Falco <isidore@setgame.com>
2020-03-09 22:26:29 -0400

    koans from todays learning hour

--- src/test/java/org/koans/Lesson1RequireNotNull.java
+++ src/test/java/org/koans/Lesson1RequireNotNull.java
@@ -2,9 +2,11 @@ package org.koans;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 import org.junit.Test;
+import org.koans.helpers.HashMap___;
 import org.koans.helpers.Koans;
 
 public class Lesson1RequireNotNull extends Koans
@@ -43,6 +45,35 @@ public class Lesson1RequireNotNull extends Koans
       e.printStackTrace();
       assertEquals(___, e.getStackTrace()[1].getLineNumber());
     }
+  }
+  @Test
+  public void hashes() throws Exception
+  {
+    String name = "Zach";
+    int number = 42;
+    double pi = 3.14;
+    int hash = Objects.hash(name, number, ___);
+    assertEquals(-1325618168, hash);
+  }
+  @Test
+  public void binaryAndUnderscores() throws Exception
+  {
+    int answer = 0b0_0010_1010;
+    assertEquals(___, answer);
+  }
+  @Test
+  public void binaryFlags() throws Exception
+  {
+    int starting = 0b0_0111_1111;
+    int turnOffEveryOther = ____;
+    int answer = starting ^ turnOffEveryOther;
+    assertEquals(42, answer);
+  }
+  @Test
+  public void diamondOperator() throws Exception
+  {
+    // get this to compile.
+    HashMap<Integer, HashMap<String, Integer>> map = new HashMap___();
   }
   /********************************** helper code below ********************/
   /**
---
+++ src/test/java/org/koans/helpers/HashMap___.java
@@ -0,0 +1,7 @@
+package org.koans.helpers;
+
+import java.util.HashMap;
+
+public class HashMap___ extends HashMap<Boolean, Long>
+{
+}
